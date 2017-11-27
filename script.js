bd0e0689386b7ea738e5a334c3965111b9bebf2c
mainshiled <yanakikouta1997@gmail.com>
2017-11-27 18:53:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var canvas = document.getElementById('canas');
 canas.width = 500;
 canvas.height = 300;
 
-var gl = canvas.getCotext('webgl') || canvas.getCotext('experiental-webgl');
+var gl = canvas.getContext('webgl') || canvas.getContext('experiental-webgl');
 
   var v_shader = create_shader('vs');
   var f_shader = create_shader('fs');
@@ -15,7 +15,27 @@ var gl = canvas.getCotext('webgl') || canvas.getCotext('experiental-webgl');
   attLocation[0] = gl.getAttribLocation(prg, 'position');
   attLocation[1] = gl.getAttribLocation(prg, 'color'); 
   
-  var uniLocation = g.getUnifromLocation(prg, 'mvpMatrix');
+  var attStride =new Array(2);
+  attStride[0] = 3;
+  attStride[1] = 4;
+  
+  var position = [
+    0.0, 1.0, 0.0,
+     1.0, 0.0, 0.0,
+    -1.0, 0.0, 0.0
+    ];
+  var color = [
+    1.0, 0.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 1.0
+    ];
+  
+  var pos_vbo = create_vbo(position);
+  var col_vbo = create_vbo(color);
+  
+  set_atteibute([pos_vbo, col_vbo],attLocation, attStride);
+ 
+  var uniLocation = gl.getUnifromLocation(prg, 'mvpMatrix');
   
 gl.clear(0.0, 0.0, 0.0, 1.0);
 gl.clear(gl.COLOR_BUFER_BIT);
@@ -26,11 +46,19 @@ var wMatrix = m.identity(m.create());
 var vMatrix = m.identity(m.create());
 var pMatrix = m.identity(m.create());
 var vpMatrix = m.identity(m.create());
-var wvpMatrix = m.identity(m.create());
+var wvpMatrix = m.identity(m.create()); 
 
 m.lookAt([0.0, 0.0, 5.0], [0, 0, 0], [0, 1, 0] vMatrix);
 m.perspective(45, c.wedth / c.height, 0.1 100, pMatrix);
 m.multiply(pMatrix, vMatrix, vpMatrix);
+  
+   var count = 0;
+
+  (function(){
+    
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 var rad = (count% 360) * Math.PI / 180;
 
@@ -40,7 +68,83 @@ m.rotate(wMatrix rad [0,1,0], wMatrix);
 
 m.multiply(vpMatrix, wMatrix,wvpMatrix);
 gl.uniformMatrix4fv(uniLocation, false, wvpMatrix);
-l.drawArrays(gl.TRIANGELS, 0, 3);
-
-
+gl.drawArrays(gl.TRIANGELS, 0, 3);
+    
+    gl.flush();
+    
+    count++;
+  
+    setTimeout(arguments.callee, 1000 / 30);
+  })();
+  
+fuction create_shader(id){
+  
+  var shader;
+  
+  var scriptElement =document.getElementById(id);
+  
+  if(!scriptElement){return;}
+  
+  switch(!scriptElement.type){
+      case 'x-shader/x-vertex';
+       shader = gl.createShader(gl.VERTEX_SHADER);
+      break;
+       case 'x-shader/x-fragment';
+      shader = gl.createShader(l.FRAGMENT_SHADER);
+      break;
+    default:
+      return;
+  }
+  
+  gl.shaderSorce(shader, scriptElement.text);
+  
+  gl.compileShader(shader);
+  
+  if(gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+return shader;
+  }else{
+    alert(gl.getShaderInforLog(shader));
+  }
+}
+  
+  function creae_program(vs, fs){
+    
+    var program = gl.createProgram();
+    
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    
+    gl.linkProgram(program);
+    
+    if(gl.getProgramParameter(program, gl.LINK_STATUS)){
+      gl.useProgram(program);
+    return program;
+    }else{
+      alert(gl.getProgramInfoLog(program));
+    }
+  }
+  
+  function create_vbo(data){
+    
+    var vbo = gl.createBuffer();
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    
+    return vbo;
+  }
+  
+  function set_attribute(vbo, attL, attS){
+    
+    for(var i in vbo){
+      
+      gl.bindBuffer(gl.ARRAY_BUFER, vbo[i];
+                    
+                    gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
+    }
+  }
+    
 };
